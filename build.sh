b716b9245f03314da3663f1f084a05e9afb69f3e
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2023-07-14 13:28:50 +0000

    Update Dockerfile, build.sh

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ groupID=$(id -g)
 
 mkdir -p ./build
 
-echo "Building Docker image"
-imageID=$(docker build -f docker/Dockerfile --build-arg UID="${userID}" --build-arg GID="${groupID}" -q .)
+echo "Building Docker image & Running build"
+imageID=$(docker build -f docker/Dockerfile --build-arg UID="${userID}" --build-arg GID="${groupID}" --target builder -q .)
 
-echo "Running build"
-docker run --rm -v ./build:/build "$imageID"
+echo "Copying output"
+docker run --rm -v ./build:/target "$imageID" cp /build/docker_launcher /target
 
 echo "Removing image"
 docker image rm "$imageID"
